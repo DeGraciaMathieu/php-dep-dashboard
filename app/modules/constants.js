41c34b0a21381ef99c747c921aef825782b503b6
DeGraciaMathieu <degracia.mathieu@gmail.com>
2026-03-05 09:44:19 +0100

    Add constants module (EVENTS, DOM, NODE_TYPE)

---
+++ app/modules/constants.js
@@ -0,0 +1,42 @@
+export const EVENTS = {
+  DATA_LOADED: 'data:loaded',
+  NODE_SELECTED: 'node:selected',
+  EDGE_SELECTED: 'edge:selected',
+  SELECTION_CLEARED: 'selection:cleared',
+  FOCUS_NODE: 'focus:node',
+  FOCUS_RESET: 'focus:reset',
+  LAYOUT_RUN: 'layout:run',
+  FILTERS_APPLIED: 'filters:applied',
+  NAMESPACE_REBUILD: 'namespace:rebuild',
+  GRAPH_READY: 'graph:ready',
+};
+
+export const DOM = {
+  CY: 'cy',
+  FILTERS: 'filters',
+  DETAIL_PANEL: 'detail-panel',
+  NS_BREADCRUMB: 'ns-breadcrumb',
+  NS_PANEL: 'ns-panel',
+  SEARCH_INPUT: 'search-input',
+  SEARCH_RESULTS: 'search-results',
+  WARNINGS_BADGE: 'warnings-badge',
+  WARNINGS_LIST: 'warnings-list',
+  META_STATS: 'meta-stats',
+  LOADING: 'loading',
+  LARGE_DATASET_BANNER: 'large-dataset-banner',
+  GRAPH_STATUS: 'graph-status',
+  BTN_NEW_ANALYSIS: 'btn-new-analysis',
+  DROP_ZONE: 'drop-zone',
+  LAYOUT_SELECT: 'layout-select',
+  NS_SELECT_ALL: 'ns-select-all',
+  NS_DESELECT_ALL: 'ns-deselect-all',
+  NS_SEARCH: 'ns-search',
+  FILTER_EXTERNAL: 'filter-external',
+  BTN_RESET_FOCUS: 'btn-reset-focus',
+  BTN_EXPORT_PNG: 'btn-export-png',
+};
+
+export const NODE_TYPE = {
+  NAMESPACE: 'namespace',
+  CLASS: 'class',
+};
